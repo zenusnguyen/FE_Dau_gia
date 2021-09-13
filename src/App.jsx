33cf92aa559763c465bee4cf103488088d6ef8c4
ITC - APP - Mobile Developer NGUYỄN VIỆT ANH <anh.nguyen28@mservice.com.vn>
2021-09-13 23:58:37 +0700

    init login&register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import styles from "./App.less"
+import styles from "./App.less";
+import io from "socket.io-client";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+const STRAPI_ENDPOINT = "http://localhost:1337";
 
 function App() {
   return (
     <div className={styles.App}>
-     hello world!
+      <RegisterPage></RegisterPage>
     </div>
   );
 }
---
+++ src/pages/RegisterPage/index.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { Button, Input, Form, message, Card } from "antd";
+import styles from "./index.css";
+
+export default function LoginPage() {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+    
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <div className="container">
+      <Card className="formWrapper">
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Email"
+            name="email"
+            type="email"
+            rules={[
+              { required: true, message: "Please input your email!" },
+              {
+                type: "email",
+                message: "Please enter your email",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            label="Confirm Password"
+            name="confirmPassword"
+            rules={[
+              { required: true, message: "Please input your password!" },
+              ({ getFieldValue }) => ({
+                validator(__, value) {
+                  if (!value || getFieldValue("password") === value)
+                    return Promise.resolve();
+                  return Promise.reject(new Error("Password do not match"));
+                },
+              }),
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
+}
