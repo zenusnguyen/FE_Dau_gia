0b2f9b8619c35a6d81327a4a69749d661c2644dc
zenus <zenusnguyen@Gmail.com>
2021-10-15 05:10:31 +0700

    update product management add i solded

--- src/pages/ProductManagement/index.jsx
+++ src/pages/ProductManagement/index.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import styles from "./styles.module.css";
+import ProductImage from "../../assets/product.svg";
+import ProfileProductItem from "../../components/ProfileProductItem";
+const products = [
+  {
+    id: "5",
+    title:
+      "Taylor Swift - Frealess (Taylor's Version) (Metallic Gold Vinyl) [3LP]",
+    price: "1.250.000đ",
+    view: "20",
+    timming: "12d 8h 5m",
+    src: ProductImage,
+    width: 240,
+    bidder: { name: "***Anh", percent: 80 },
+    postingDate: "02/09/2021 10:30",
+    auctionMoney: "1.300.000đ",
+    buyNow: "1.500.000đ",
+    status: "processing",
+  },
+  {
+    id: "5",
+    title:
+      "Taylor Swift - Frealess (Taylor's Version) (Metallic Gold Vinyl) [3LP]",
+    price: "1.250.000đ",
+    view: "20",
+    timming: "12d 8h 5m",
+    src: ProductImage,
+    width: 240,
+    bidder: { name: "***Anh", percent: 80 },
+    postingDate: "02/09/2021 10:30",
+    auctionMoney: "1.300.000đ",
+    buyNow: null,
+    status: "sold",
+  },
+];
 
 export default function ProductManagement() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <div>
+        {products.map((product) => (
+          <ProfileProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            view={product.view}
+            timming={product.timming}
+            src={product.src}
+            width={product.width}
+            bidder={product.bidder}
+            postingDate={product.postingDate}
+            auctionMoney={product.auctionMoney}
+            buyNow={product.buyNow}
+            status={product?.status}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/pages/ProfileManagement/index.jsx
+++ src/pages/ProfileManagement/index.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import SideMenu from "../../components/SideMenu";
 import styles from "./styles.module.css";
+import ProductManagement from "../ProductManagement";
 
 export default function ProfileManagement() {
+  const [currentKey, setCurrentKey] = useState("4");
+
+  const renderComponent = () => {
+    switch (currentKey) {
+      case "1":
+        // code block
+        break;
+      case "2":
+        // code block
+        break;
+      case "3":
+        // code block
+        break;
+      case "6":
+        return <ProductManagement></ProductManagement>;
+        break;
+      default:
+      // code block
+    }
+  };
+
   return (
     <div className={styles.container}>
-      <SideMenu></SideMenu>
+      <SideMenu handleClick={setCurrentKey}></SideMenu>
+      {renderComponent()}
     </div>
   );
 }
