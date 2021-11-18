8a5f6e9caf6fdd26e45a5eee8c036a72ad973800
Mobile App Developer NGUYỄN VIỆT ANH <anh.nguyen28@mservice.com>
2021-11-18 19:44:48 +0700

    update full text search

--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -6,76 +6,80 @@ import ProductList from "../../components/ProductList";
 import Text from "../../components/Text";
 import { getCountSearch, search } from "../../services/productApi";
 export default function SearchPage() {
-   const history = useHistory();
-   const { searchWord, pageNumber } = useParams();
-   const [breadcrumb, setBreadcrumb] = useState([]);
-   const [items, setItems] = useState([]);
-   const [currentPage, setCurrentPage] = useState(1);
-   const [toTalPage, setToTalPage] = useState(1);
-   const [isLoading, setIsLoading] = useState(true);
+  const history = useHistory();
+  const { searchWord, pageNumber } = useParams();
+  const [breadcrumb, setBreadcrumb] = useState([]);
+  const [items, setItems] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [toTalPage, setToTalPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
 
-   useEffect(() => {
-      const fetchData = async () => {
-         setBreadcrumb(["Tiềm kiếm", searchWord]);
-         //HandleApi
-      };
-      fetchData();
-   }, [searchWord, currentPage, pageNumber]);
+  useEffect(() => {
+    const fetchData = async () => {
+      setBreadcrumb(["Tiềm kiếm", searchWord]);
+      console.log("searchWord: ", searchWord);
+      await search(searchWord).then((res) => {
+         console.log('res: ', res);
+        setItems(res);
+      });
+    };
+    fetchData();
+  }, [searchWord, currentPage, pageNumber]);
 
-   const handlePageChange = (pageNumber) => {
-      setCurrentPage(pageNumber);
-      history.push(`/search/${searchWord}/page/${pageNumber}`);
-   };
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+    history.push(`/search/${searchWord}/page/${pageNumber}`);
+  };
 
-   return (
-      <div className={styles.searchPage}>
-         <div className={styles.searchPageContainer}>
-            <div className={styles.content}>
-               <Breadcrumb style={{ marginBottom: "30px" }}>
-                  <Breadcrumb.Item>
-                     <Text.caption title="Trang chủ" />
-                  </Breadcrumb.Item>
-                  {breadcrumb.map((item) => (
-                     <Breadcrumb.Item>
-                        <Text.caption title={item} />
-                     </Breadcrumb.Item>
-                  ))}
-               </Breadcrumb>
-               <div className={styles.contentTop}>
-                  <div className={styles.filter}>
-                     <Button
-                        style={{
-                           borderTopRightRadius: "0",
-                           borderBottomRightRadius: "0",
-                        }}
-                     >
-                        <Text.caption title="Thời gian kết thúc giảm dần" />
-                     </Button>
-                     <Button
-                        style={{
-                           borderTopLeftRadius: "0",
-                           borderBottomLeftRadius: "0",
-                        }}
-                     >
-                        <Text.caption title="Giá tăng dần" />
-                     </Button>
-                  </div>
-                  <Pagination
-                     defaultCurrent={currentPage}
-                     total={toTalPage}
-                     pageSize={2}
-                  />
-               </div>
-               <ProductList products={items} />
-               <div className={styles.contentBottom}>
-                  <Pagination
-                     defaultCurrent={currentPage}
-                     total={toTalPage}
-                     pageSize={2}
-                  />
-               </div>
+  return (
+    <div className={styles.searchPage}>
+      <div className={styles.searchPageContainer}>
+        <div className={styles.content}>
+          <Breadcrumb style={{ marginBottom: "30px" }}>
+            <Breadcrumb.Item>
+              <Text.caption title="Trang chủ" />
+            </Breadcrumb.Item>
+            {breadcrumb.map((item) => (
+              <Breadcrumb.Item>
+                <Text.caption title={item} />
+              </Breadcrumb.Item>
+            ))}
+          </Breadcrumb>
+          <div className={styles.contentTop}>
+            <div className={styles.filter}>
+              <Button
+                style={{
+                  borderTopRightRadius: "0",
+                  borderBottomRightRadius: "0",
+                }}
+              >
+                <Text.caption title="Thời gian kết thúc giảm dần" />
+              </Button>
+              <Button
+                style={{
+                  borderTopLeftRadius: "0",
+                  borderBottomLeftRadius: "0",
+                }}
+              >
+                <Text.caption title="Giá tăng dần" />
+              </Button>
             </div>
-         </div>
+            <Pagination
+              defaultCurrent={currentPage}
+              total={toTalPage}
+              pageSize={2}
+            />
+          </div>
+          <ProductList products={items} />
+          <div className={styles.contentBottom}>
+            <Pagination
+              defaultCurrent={currentPage}
+              total={toTalPage}
+              pageSize={2}
+            />
+          </div>
+        </div>
       </div>
-   );
+    </div>
+  );
 }
